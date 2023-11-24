e83887cb9bf708a7c19498ee07234d055363c3d8
Shailandra Rajput <shailandra711@gmail.com>
2023-11-24 23:01:25 +0530

    Dice Practical

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,28 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import '../utils/methods/common_methods.dart';
-import '../widgets/info_tile.dart';
-
-class HelloPage extends StatelessWidget {
+class HelloPage extends StatefulWidget {
   const HelloPage({super.key});
+
+  @override
+  State<HelloPage> createState() => _HelloPageState();
+}
+
+class _HelloPageState extends State<HelloPage> {
+  int currentDice = 1;
+  void roleDice() {
+    Random random = Random();
+
+    setState(() {
+      currentDice = random.nextInt(5) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple,
+      backgroundColor: Colors.black,
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
@@ -18,58 +30,16 @@ class HelloPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.purple.shade700, shape: BoxShape.circle),
-              child: ClipOval(
-                child: Image.asset(
-                  "assets/images/unnamed.png",
-                  height: 100,
-                  width: 100,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Text(
-              "Amir Hossain Bayat",
-              style: GoogleFonts.getFont('Pacifico',
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-            Text(
-              "iOS & Android Developer",
-              style: GoogleFonts.getFont('Poppins',
-                  fontSize: 18, letterSpacing: 3.0, color: Colors.white),
-            ),
-            const Divider(
-              color: Colors.white,
-              height: 20,
-              indent: 100,
-              endIndent: 100,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            InfoTile(
-              icon: Icons.phone,
-              text: "+98 922 505 8169",
-              function: () {
-                CommonMethods().operUrls("tel:918511224041");
+            GestureDetector(
+              onTap: () {
+                roleDice();
               },
-            ),
-            InfoTile(
-                icon: Icons.email,
-                text: "AmirBayat.dev@gmail.com",
-                function: () {
-                  CommonMethods().operUrls("mailto:sr@grr.la");
-                }),
-            InfoTile(
-                icon: Icons.account_circle,
-                text: "@CodeWithflexz",
-                function: () {
-                  CommonMethods().operUrls("https://wa.me/918511224041");
-                }),
+              child: Image.asset(
+                "assets/images/$currentDice.png",
+                height: 160,
+                width: 160,
+              ),
+            )
           ],
         ),
       ),
